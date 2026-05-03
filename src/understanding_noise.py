3e486bdad89c89030e4775426e08f449ba32743a
Carolina Barroco <carolpbarroco@gmail.com>
2026-05-03 17:39:28 -0300

    immerkaer_calc added

--- src/understanding_noise.py
+++ src/understanding_noise.py
@@ -1,4 +1,5 @@
 import utils
+import noise_filter
 from pathlib import Path
 import cv2
 import matplotlib.pyplot as plt
@@ -7,6 +8,10 @@ import numpy as np
 
 def understanding_noise():
     raw_img_list = []
+    img_median_applied_list= []
+    img_gaussian_applied_list= []
+    img_bilateral_applied_list = []
+    img_blur_applied_list =[]
 
     masks_comparison_dice = []
     datadir = Path("data/images/images")
@@ -21,9 +26,23 @@ def understanding_noise():
         if image is None:
             continue
         raw_img_list.append(image.astype(np.float64))
+        img_median_applied_list.append(noise_filter.filter_img(image, "median"))
+        img_gaussian_applied_list.append(noise_filter.filter_img(image, "gaussian"))
+        img_bilateral_applied_list.append(noise_filter.filter_img(image, "bilateral"))
+        img_blur_applied_list.append(noise_filter.filter_img(image, "blur"))
 
-    all_pixels = np.concatenate(raw_img_list)
-    utils.variance_vs_intensity_from_pixels(all_pixels)
+    
+     
+    
+    utils.immerkaer_calc(np.concatenate(raw_img_list))
+
+    #utils.variance_vs_intensity_from_image(np.concatenate(raw_img_list))
+    # utils.variance_vs_intensity_from_pixels(np.concatenate(raw_img_list), np.concatenate(img_median_applied_list))
+    # utils.variance_vs_intensity_from_pixels(np.concatenate(raw_img_list), np.concatenate(img_gaussian_applied_list))
+    # utils.variance_vs_intensity_from_pixels(np.concatenate(raw_img_list), np.concatenate(img_bilateral_applied_list))
+    # utils.variance_vs_intensity_from_pixels(np.concatenate(raw_img_list), np.concatenate(img_blur_applied_list))
+
+
     
     
   
